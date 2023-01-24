22db03929c3590b2f63b02e8f3fe952cb037df5c
Andrei Pivoda <andrei.pivoda@gmail.com>
2023-01-24 20:45:45 +0200

    change class to function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,12 @@
-import React, { Component } from 'react';
 
-export default class App extends Component {
-    static displayName = App.name;
+import React, { useState, useEffect } from 'react'
 
-    constructor(props) {
-        super(props);
-        this.state = { forecasts: [], loading: true };
-    }
+export default function App() {
 
-    componentDidMount() {
-        this.populateWeatherData();
-    }
+    const [forecasts, setForecasts] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    static renderForecastsTable(forecasts) {
+    const RenderForecastsTable = () => {
         return (
             <table className='table table-striped' aria-labelledby="tabelLabel">
                 <thead>
@@ -36,24 +30,35 @@ export default class App extends Component {
             </table>
         );
     }
-
-    render() {
-        let contents = this.state.loading
-            ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-            : App.renderForecastsTable(this.state.forecasts);
-
+    const IsLoading = () => {
         return (
-            <div>
-                <h1 id="tabelLabel" >Weather forecast</h1>
-                <p>This component demonstrates fetching data from the server.</p>
-                {contents}
-            </div>
+            <p>Loading... </p>
         );
     }
 
-    async populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        const data = await response.json();
-        this.setState({ forecasts: data, loading: false });
-    }
+    useEffect(() => {
+        const populateWeatherData = async () => {
+            const response = await fetch('weatherforecast');
+            const data = await response.json();
+            setTimeout(() => {
+                setForecasts(data);
+                setLoading(false);
+            }, 2000);
+
+        }
+        populateWeatherData();
+
+
+        return () => {
+
+        }
+    }, [])
+
+
+    return (
+        <div>{loading ? <IsLoading /> : <RenderForecastsTable />}
+        </div>
+    )
 }
+
+
